dfafb3e7090371056b03afa2eb55dcae3c4baae3
William Robertson <wr222ae@student.lnu.se>
2017-01-08 21:12:23 +0100

    adding memory to app

--- examination/client/source/js/Memory.js
+++ examination/client/source/js/Memory.js
@@ -8,7 +8,107 @@ const GUI = require('./GUI');
 class Memory extends GUI{
     constructor(name, count){
         super(name, count);
+        let content = document.getElementById(name+count).lastElementChild;
+        this.rows = 4;
+        this.cols = 4;
+        let tiles = this.picArray(4, 4);
+        this.gameBoard(4, content, tiles);
+        this.turn1;
+        this.turn2;
+        this.lastTile;
+        this.pairs = 0;
+        this.tries = 0;
     }
+
+    gameBoard(cols, container, tiles) {
+        let aTag;
+        // container = document.getElementById(container);
+        let template = document.querySelectorAll('#memoryContainer template')[0].content.firstElementChild;
+
+        for(let i = 0; i < tiles.length; i++){
+            aTag = document.importNode(template.firstElementChild, true);
+
+            container.appendChild(aTag);
+
+            let tile = tiles[i];
+
+            aTag.addEventListener('click', event =>{
+                let img = event.target.nodeName === 'IMG' ? event : event.firstElementChild;
+                this.turnBrick(tile, event.target)
+            });
+
+            if((i + 1) % cols === 0){
+                container.appendChild(document.createElement('br'));
+            }
+        }
+    }
+
+    picArray(rows, cols) {
+        let arr = [];
+
+        for(let i = 1; i <= (rows * cols) / 2; i++){                    //Creates an array with the amount of cards that the client has chosen
+            arr.push(i);
+            arr.push(i);
+        }
+
+        let n = arr.length;
+        let shuffledArr = [];
+
+        while (n) {                                                     //Shuffles the array
+            let i = Math.floor(Math.random() * n--);
+            shuffledArr.push(arr.splice(i, 1)[0]);
+        }
+
+        return shuffledArr;
+    }
+
+    turnBrick(tile, img) {
+
+        if(this.turn2){
+            return;
+        }
+
+        img.src = 'image/' + tile + '.png';
+
+        if(!this.turn1){
+            this.turn1 = img;
+            this.lastTile = tile;
+            return;
+
+        }else{
+            if(img === this.turn1){
+                return;
+            }
+            this.tries += 1;
+            this.turn2 = img;
+
+            if(tile === this.lastTile){
+                this.pairs += 1;
+
+                if(this.pairs === (this.rows * this.cols) / 2){
+                    console.log('You won on ' + this.tries + ' tries!!');
+                }
+                setTimeout(timeOut =>{
+                    this.turn1.parentNode.classList.add('pair');
+                    this.turn2.parentNode.classList.add('pair');
+
+                    this.turn1 = null;
+                    this.turn2 = null;
+
+                },500);
+
+            }else{
+                window.setTimeout( e =>{
+                    this.turn1.src = 'image/0.png';
+                    this.turn2.src = 'image/0.png';
+
+                    this.turn1 = null;
+                    this.turn2 = null;
+                }, 500)
+            }
+        }
+    }
+
 }
 
 module.exports = Memory;
